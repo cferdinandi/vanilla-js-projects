080f40689fd0ea99ff773d6254e40b7a791c3ebb
cferdinandi <chris@gomakethings.com>
2019-04-22 15:39:24 -0400

    Initial commit

---
+++ themes/projects/src/js/main/zzz_init.js
@@ -0,0 +1,21 @@
+/**
+ * Script initializations
+ */
+
+// Responsive iframe videos
+fluidvids.init({
+	selector: ['iframe', 'object'],
+	players: ['www.youtube.com', 'player.vimeo.com']
+});
+
+// Mailchimp form
+if (document.querySelector('#mailchimp-form')) {
+	mailchimp(function (data) {
+		if (data.code === 200) {
+			window.location.href = '/signup-success';
+		}
+	});
+}
+
+// Pricing parity
+pricingParity('https://gomakethings.com/checkout/wp-json/gmt-pricing-parity/v1/discount/', '<div class="container container-large"><img width="100" style="float:left;margin: 0 16px 16px 0;" src="https://flagpedia.net/data/flags/normal/{{iso}}.png"><p class="text-small no-margin-bottom">Hi! Looks like you\'re from <strong>{{country}}</strong>, where the <strong>Vanilla JS Academy</strong> might be a bit expensive. You can use the code <strong>{{code}}</strong> at checkout to take <strong>{{amount}}</strong> off the program. Cheers!</p></div>');
