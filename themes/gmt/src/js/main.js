6859a4ec7b5fdcb9c7c55c064c73edb026204928
cferdinandi <chris@gomakethings.com>
2020-04-29 15:28:54 -0400

    Updated build and theme

---
+++ themes/gmt/src/js/main.js
@@ -0,0 +1,23 @@
+import './main-components/_matches-polyfill.js';
+import mailchimp from './main-components/mailchimp.js';
+import addHeadingLinks from './main-components/heading-links.js';
+import filterTopics from './main-components/filters.js';
+
+// Mailchimp form
+if (document.querySelector('#mailchimp-form')) {
+	mailchimp(function (data) {
+		if (data.code === 200) {
+			window.location.href = 'https://gomakethings.com/newsletter-success';
+		}
+	});
+}
+
+// Anchor links on posts
+if (document.body.matches('.js-anchors')) {
+	addHeadingLinks('h2, h3, h4, h5, h6', '#', 'link-no-underline');
+}
+
+// Filter topics
+if (document.querySelector('[data-filter]')) {
+	filterTopics();
+}
