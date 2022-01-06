6ec067928f75e5501574b69e4216148dbde3955a
cferdinandi <chris@gomakethings.com>
2022-01-06 13:46:24 -0500

    Switched to ConvertKit

--- themes/gmt/src/js/main.js
+++ themes/gmt/src/js/main.js
@@ -1,23 +1,4 @@
-import '../../../../../gmt-theme/dist/js/_matches.polyfill.js';
-import mailchimp from '../../../../../gmt-theme/dist/js/mailchimp.js';
-// import addHeadingLinks from '../../../../../gmt-theme/dist/js/heading-links.js';
-// import filterTopics from './main-components/filters.js';
+import convertkit from '../../../../../gmt-theme/dist/js/convertkit.js';
 
-// Mailchimp form
-if (document.querySelector('#mailchimp-form')) {
-	mailchimp(function (data) {
-		if (data.code === 200) {
-			window.location.href = 'https://gomakethings.com/newsletter-success';
-		}
-	});
-}
-
-// // Anchor links on posts
-// if (document.body.matches('.js-anchors')) {
-// 	addHeadingLinks('h2, h3, h4, h5, h6', '#', 'link-no-underline');
-// }
-
-// // Filter topics
-// if (document.querySelector('[data-filter]')) {
-// 	filterTopics();
-// }
+// ConvertKit form
+convertkit();
