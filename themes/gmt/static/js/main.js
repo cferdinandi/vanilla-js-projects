99b9635017e30955f64bdfa3b9dc6e5789238a8d
cferdinandi <chris@gomakethings.com>
2022-03-05 16:36:43 -0500

    Updated guides

--- themes/gmt/static/js/main.js
+++ themes/gmt/static/js/main.js
@@ -165,70 +165,7 @@
 
 	}
 
-	/**
-	 * Load pricing parity message
-	 */
-	function pricingParity (endpoint, template) {
-
-		// Make sure endpoint and template exist
-		if (!endpoint) return;
-
-		// Render the pricing parity message
-		function renderPricingParity (data) {
-
-			// Make sure we have data and a template to render
-			if (!data || !template) return;
-
-			// Make sure discount exists
-			if (data.status === 'no_discount') return;
-
-			// Get the nav
-			let nav = document.querySelector('header');
-			if (!nav) return;
-
-			// Create container
-			let pricing = document.createElement('div');
-			pricing.id = 'pricing-parity';
-			pricing.className = 'bg-muted padding-top-small padding-bottom-small';
-			pricing.innerHTML = template.replace('{{iso}}', data.code).replace('{{country}}', data.country).replace('{{code}}', data.discount).replace('{{amount}}', data.amount);
-
-			// Insert into the DOM
-			nav.parentNode.insertBefore(pricing, nav);
-
-		}
-
-		// Get the pricing parity message via Ajax
-		function getPricingParity () {
-			fetch(endpoint).then(function (response) {
-				if (response.ok) {
-					return response.json();
-				}
-				return Promise.reject(response);
-			}).then(function (data) {
-
-				// Save the content to sessionStorage
-				sessionStorage.setItem('gmt-location-pricing', JSON.stringify(data));
-
-				// Render it
-				renderPricingParity(data);
-
-			});
-		}
-
-		// Get and render pricing parity info
-		let pricing = sessionStorage.getItem('gmt-location-pricing');
-		if (pricing) {
-			renderPricingParity(JSON.parse(pricing));
-		} else {
-			getPricingParity();
-		}
-
-	}
-
 	// ConvertKit form
 	convertkit();
-
-	// Pricing parity
-	pricingParity('https://gomakethings.com/checkout/wp-json/gmt-pricing-parity/v1/discount/', '<div class="container container-large"><img width="100" style="float:left;margin: 0.125em 1em 1em 0;" src="https://flagpedia.net/data/flags/normal/{{iso}}.png"><p class="text-small no-margin-bottom">Hi! Looks like you\'re from <strong>{{country}}</strong>, where my <strong>Vanilla JS Pocket Guides</strong> might be a bit expensive. A <strong>{{amount}}% discount</strong> will automatically be applied to every guide, course, and bundle at checkout. Cheers!</p></div>');
 
 }());
